0c123fa1463cc8d61fcebd49a2a5c179b83cb034
Pierre Grandin <grandinp@altern.org>
2014-02-15 23:22:24 -0800

    Working on the plugin

--- spotify.c
+++ spotify.c
@@ -66,18 +66,19 @@ try_jukebox_start (void)
   sp_track *t;
 
   if (!g_jukeboxlist)
-    printf ("jukebox: No playlist. Waiting\n");
-  // return;
+    dbg (0, "jukebox: No playlist. Waiting\n");
+    // Fixme : g_jukeboxlist is never set to the right value
+    //return;
 
   if (!sp_playlist_num_tracks (g_jukeboxlist))
     {
-      printf ("jukebox: No tracks in playlist. Waiting\n");
+      dbg (0,"jukebox: No tracks in playlist. Waiting\n");
       return;
     }
 
   if (sp_playlist_num_tracks (g_jukeboxlist) < g_track_index)
     {
-      printf ("jukebox: No more tracks in playlist. Waiting\n");
+      dbg (0,"jukebox: No more tracks in playlist. Waiting\n");
       return;
     }
 
@@ -102,8 +103,7 @@ try_jukebox_start (void)
 
   g_currenttrack = t;
 
-  printf ("jukebox: Now playing \"%s\"...\n", sp_track_name (t));
-  fflush (stdout);
+  dbg (0,"jukebox: Now playing \"%s\"...\n", sp_track_name (t));
 
   sp_session_player_load (g_sess, t);
   sp_session_player_play (g_sess, 1);
@@ -145,7 +145,7 @@ playlist_added (sp_playlistcontainer * pc, sp_playlist * pl,
 static void
 container_loaded (sp_playlistcontainer * pc, void *userdata)
 {
-  fprintf (stderr, "jukebox: Rootlist synchronized (%d playlists)\n",
+  dbg (0, "jukebox: Rootlist synchronized (%d playlists)\n",
 	   sp_playlistcontainer_num_playlists (pc));
   try_jukebox_start ();
 }
@@ -165,7 +165,7 @@ on_login (sp_session * session, sp_error error)
   dbg (0, "spotify login\n");
   if (error != SP_ERROR_OK)
     {
-      fprintf (stderr, "Error: unable to log in: %s\n",
+      dbg (0,stderr, "Error: unable to log in: %s\n",
 	       sp_error_message (error));
       exit (1);
     }
@@ -175,14 +175,114 @@ on_login (sp_session * session, sp_error error)
   int i;
 
   sp_playlistcontainer_add_callbacks (pc, &pc_callbacks, NULL);
-dbg (0, "Got %d playlists\n", sp_playlistcontainer_num_playlists (pc))}
+  dbg (0, "Got %d playlists\n", sp_playlistcontainer_num_playlists (pc))
+
+  for (i = 0; i < sp_playlistcontainer_num_playlists (pc); ++i)
+    {
+      sp_playlist *pl = sp_playlistcontainer_playlist (pc, i);
+
+      sp_playlist_add_callbacks (pl, &pl_callbacks, NULL);
+
+      if (!strcasecmp (sp_playlist_name (pl), g_listname))
+        {
+          dbg (0,"Found the playlist %s\n", g_listname);
+          switch (sp_playlist_get_offline_status (session, pl))
+            {
+            case SP_PLAYLIST_OFFLINE_STATUS_NO:
+              dbg (0, "Playlist is not offline enabled.\n");
+              sp_playlist_set_offline_mode (session, pl, 1);
+              dbg (0, "  %d tracks to sync\n",
+                      sp_offline_tracks_to_sync (session));
+              break;
+
+            case SP_PLAYLIST_OFFLINE_STATUS_YES:
+              dbg (0, "Playlist is synchronized to local storage.\n");
+              break;
+
+            case SP_PLAYLIST_OFFLINE_STATUS_DOWNLOADING:
+              dbg
+                (0, "This playlist is currently downloading. Only one playlist can be in this state any given time.\n");
+              break;
+
+            case SP_PLAYLIST_OFFLINE_STATUS_WAITING:
+              dbg (0, "Playlist is queued for download.\n");
+              break;
+
+            default:
+              dbg (0, "unknow state\n");
+              break;
+            }
+          g_jukeboxlist = pl;
+          try_jukebox_start ();
+        }
+    }
+  if (!g_jukeboxlist)
+    {
+      dbg (0, "jukebox: No such playlist. Waiting for one to pop up...\n");
+    }
+  try_jukebox_start ();
+
+
+}
+
+static int
+on_music_delivered (sp_session * session, const sp_audioformat * format,
+                    const void *frames, int num_frames)
+{
+  audio_fifo_t *af = &g_audiofifo;
+  audio_fifo_data_t *afd;
+  size_t s;
+
+  s = num_frames * sizeof (int16_t) * format->channels;
+  return num_frames;
+
+  if (num_frames == 0)
+    return 0;                   // Audio discontinuity, do nothing
+
+  pthread_mutex_lock (&af->mutex);
+
+  /* Buffer one second of audio */
+  if (af->qlen > format->sample_rate)
+    {
+      pthread_mutex_unlock (&af->mutex);
+
+      return 0;
+    }
+
+  s = num_frames * sizeof (int16_t) * format->channels;
+
+  afd = malloc (sizeof (*afd) + s);
+  memcpy (afd->samples, frames, s);
+
+  afd->nsamples = num_frames;
+
+  afd->rate = format->sample_rate;
+  afd->channels = format->channels;
+
+  TAILQ_INSERT_TAIL (&af->q, afd, link);
+  af->qlen += num_frames;
+
+  pthread_cond_signal (&af->cond);
+  pthread_mutex_unlock (&af->mutex);
+
+  return num_frames;
+}
+
+static void
+on_end_of_track (sp_session * session)
+{
+  // g_playback_done = 1; 
+  //
+  ++g_track_index;
+  try_jukebox_start ();
+}
 
 static sp_session_callbacks session_callbacks = {
   .logged_in = &on_login,
 //  .notify_main_thread = &on_main_thread_notified,
-//  .music_delivery = &on_music_delivered,
+  .music_delivery = &on_music_delivered,
 //  .log_message = &on_log,
-//  .end_of_track = &on_end_of_track,
+  .end_of_track = &on_end_of_track,
 //  .offline_status_updated = &offline_status_updated,
 //  .play_token_lost = &play_token_lost,
 };
