830b228fb2439eef2a0a648f7773b2c72560f1b0
Pierre Grandin <grandinp@altern.org>
2014-02-23 10:36:01 -0800

    Removed old src tree

--- src/keys.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdlib.h>
-const uint8_t g_appkey[] = {
-};
-const size_t g_appkey_size = sizeof(g_appkey);
-const char *username = "" ;
-const char *password = "" ;
-const char *g_listname = "";
